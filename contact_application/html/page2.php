2abf937f9d697cb0ab0965e1fd0c4dc0996e4c7d
ismailbouras <76954447+ismailbouras@users.noreply.github.com>
2021-03-02 16:04:17 +0100

    commit

--- contact_application/html/page2.php
+++ contact_application/html/page2.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/contact_application/css/page2.css">
+    <link rel="stylesheet" href="../css/page2.css">
     <title>Document</title>
 </head>
 <body>
     <div class="con2">
         <form id="form" method="POST" role="form">
-            <label for="name"  class=""> first Name</label>
+            <label for="name"  class="">first Name</label>
             <input type="text" name="fname" placeholder="Enter your first Name" >
             <label for="name"  class="">last Name</label>
             <input type="text" name="lname"  placeholder="Enter your last Name" >
             <label for="email" class="">Email</label>
-            <input type="email" name="Email"  placeholder="Enter your Email"  >
+            <input type="email" name="email"  placeholder="Enter your Email"  >
             <label for="adress"  class="">Adress</label>
-            <input type="text" name="Adress"  placeholder="Enter your Adress" >
+            <input type="text" name="adress"  placeholder="Enter your Adress" >
             <label for="name"  class="">Phone</label>
-            <input type="text" name="Phone"  placeholder="Enter your Phone" >
+            <input type="text" name="phone"  placeholder="Enter your Phone" >
             <div class="bot">
                 <input type="radio" name="sir1"  class="input">family
                 <input type="radio" name="sir2"  class="input">friend
@@ -30,8 +30,7 @@
             <button class="button" name="sub" >Submit</button> 
         </form>
         <?php
-        if(isset($_POST['sub'])){
-        }
+
         ?>
     </div>
 </body>
