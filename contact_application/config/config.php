2500880c7b6e54b915edd41291b6d7dc85f118dc
ismailbouras <76954447+ismailbouras@users.noreply.github.com>
2021-03-03 10:09:48 +0100

    commit

---
+++ contact_application/config/config.php
@@ -0,0 +1,13 @@
+
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "ismail";
+
+$con = mysqli_connect($host,$username,$password,$db);
+if (!$con){
+    die("error :" . mysqli_connect_error());
+}
+
+?>
--- contact_application/core/init.php
+++ contact_application/core/init.php
@@ -1,12 +1,12 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 //Include Configuration
-require_once('config/config.php');
+require_once('../config/config.php');
 //Helper Function Files
-require_once 'helpers/functions.php';
+require_once '../helpers/functions.php';
 
 /* l'utilisation des constantes prédifinies dans notre fichier config.php*/
-$con = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+$con = new mysqli($host,$username,$password,$db);
 mb_internal_encoding('UTF-8');
 
 if($con->connect_errno)
